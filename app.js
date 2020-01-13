61b7cb48ae152fa91e6a06abf85a3117a38d6557
Vuthy <suonx002@gmail.com>
2020-01-12 18:02:04 -0600

    add airbnb eslint and config keys

--- app.js
+++ app.js
@@ -7,8 +7,14 @@ const userRouter = require('./routes/userRoutes');
 const app = express();
 
 // Middlewares
-app.use(morgan('dev'));
+console.log(process.env.NODE_ENV);
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
 app.use(express.json());
+
+app.use(express.static(`${__dirname}/public`));
+
 app.use((req, res, next) => {
   console.log('hello from the middleware');
   next();
--- server.js
+++ server.js
@@ -1,4 +1,7 @@
+const dotenv = require('dotenv');
 const app = require('./app');
+
+dotenv.config({ path: './config.env' });
 
 const PORT = process.env.PORT || 5000;
 
