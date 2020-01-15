261897784ea5168c36ba2f180d1e3aa92f4d1705
Vuthy <suonx002@gmail.com>
2020-01-15 01:20:40 -0600

    add jwt cookie to header, implement rate limiting via api, add security http header, data sanitization, and prevent parameter pollution

--- controllers/authController.js
+++ controllers/authController.js
@@ -19,6 +19,16 @@ const signToken = id => {
 const createSendToken = (user, statusCode, res) => {
   const token = signToken(user._id);
 
+  const cookieOptions = {
+    expires: new Date(Date.now() + 36000),
+    httpOnly: true
+  };
+
+  if (process.env.NODE_ENV === 'production') cookieOptions.secure = true;
+  res.cookie('jwt', token, cookieOptions);
+
+  user.password = undefined;
+
   res.status(statusCode).json({
     status: 'success',
     token,
