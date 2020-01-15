4665d1c868c9c0ce14049ced03411ad7d109e916
Vuthy <suonx002@gmail.com>
2020-01-14 23:42:18 -0600

    add updateMe functionality for changing email and name

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,17 @@
 const User = require('../models/userModel');
 const catchAsync = require('../utils/catchAsync');
+const AppError = require('../utils/appError');
+
+const filterObj = (obj, ...allowedFields) => {
+  const newObj = {};
+  Object.keys(obj).forEach(el => {
+    if (allowedFields.includes(el)) {
+      newObj[el] = obj[el];
+    }
+  });
+
+  return newObj;
+};
 
 // USER HANDLERS
 exports.getAllUsers = catchAsync(async (req, res, next) => {
@@ -41,3 +53,33 @@ exports.deleteUser = (req, res) => {
     message: 'This route is not yet defined'
   });
 };
+
+exports.updateMe = catchAsync(async (req, res, next) => {
+  // 1) Create error if user POSTs password data
+  const { password, passwordConfirm } = req.body;
+
+  if (password || passwordConfirm) {
+    return next(
+      new AppError(
+        'THis route is not for password updates. Please use /updateMyPassword',
+        400
+      )
+    );
+  }
+
+  // filtered out unwanted firleds name that are not allow to be update
+  const filteredBody = filterObj(req.body, 'name', 'email');
+  // 2) Update user document
+
+  const updatedUser = await User.findByIdAndUpdate(req.user.id, filteredBody, {
+    new: true,
+    runValidators: true
+  });
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      user: updatedUser
+    }
+  });
+});
